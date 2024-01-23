206cf11b3ce721af839f00217b1dc0ebe547dbff
maaaashi <doyaseizin.7@gmail.com>
2024-01-24 01:16:51 +0900

    logo仮設置

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,11 @@
+'use client'
+
+import Image from 'next/image'
+
 export default function Home() {
-  return <main></main>
+  return (
+    <main className='min-h-screen bg-stone-200'>
+      <Image alt='log' src='/logo.svg' width={400} height={200} />
+    </main>
+  )
 }
