3009911901fb5e9afcc219c63155692198366066
maaaashi <doyaseizin.7@gmail.com>
2024-01-25 07:53:25 +0900

    add svelte component

---
+++ src/components/Fuga.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  let count = 0
+  const add = () => {
+    count += 1
+  }
+  const subtract = () => {
+    count -= 1
+  }
+</script>
+
+<div class="text-black">
+  Svelte Component {count}
+  <button class="btn" on:click={add}>+</button>
+  <button class="btn" on:click={subtract}>-</button>
+</div>
--- src/components/Hoge.tsx
+++ src/components/Hoge.tsx
@@ -1,5 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export const Hoge = () => {
-  return <button className='btn'>Hello daisyUI</button>
+  const [count, setCount] = useState(0)
+  const add = () => setCount(count + 1)
+  const sub = () => setCount(count - 1)
+  return (
+    <div className='text-black flex gap-1 items-center'>
+      <div>
+        {'React Component '}
+        {count}
+      </div>
+      <button className='btn' onClick={add}>
+        +
+      </button>
+      <button className='btn' onClick={sub}>
+        -
+      </button>
+    </div>
+  )
 }
