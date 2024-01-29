e1d1cca430f3df5097e04378ebb952eabdff5203
maaaashi <doyaseizin.7@gmail.com>
2024-01-30 08:12:05 +0900

    add h1 tag

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,9 @@ const Header = () => {
   return (
     <div className='border-b-[1px] w-screen font-bold'>
       <nav className='navbar'>
-        <button className='btn btn-ghost'>{"MAAAASHI'S WEBSITE"}</button>
+        <button className='btn btn-ghost'>
+          <h1>{"MAAAASHI'S WEBSITE"}</h1>
+        </button>
       </nav>
     </div>
   )
