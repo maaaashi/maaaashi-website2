caf94cb4cd177d9e3e9d875482e89ba0f27ba9dd
maaaashi <doyaseizin.7@gmail.com>
2024-01-29 18:34:45 +0900

    add profile image

--- src/components/profile/ProfileBox.tsx
+++ src/components/profile/ProfileBox.tsx
@@ -13,7 +13,7 @@ export const ProfileBox = () => {
       <div className='flex gap-2'>
         <div className='avatar'>
           <div className='w-24 rounded-xl'>
-            <img src='https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg' />
+            <img src='/profile.JPG' alt='IMAGE' />
           </div>
         </div>
         <div>
