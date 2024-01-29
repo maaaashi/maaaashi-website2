9d077eb1f3f4a3a6514119db25329696f244380a
maaaashi <doyaseizin.7@gmail.com>
2024-01-30 08:06:07 +0900

    add animation

--- src/components/profile/WindowMock.tsx
+++ src/components/profile/WindowMock.tsx
@@ -5,10 +5,10 @@ export const WindowMock = () => {
   return (
     <div className='mockup-window border bg-base-300'>
       <div className='flex px-4 py-4 bg-base-200 gap-4'>
-        <div className='min-w-80'>
+        <div className='min-w-80 animate-slide-down'>
           <InfoBox />
         </div>
-        <div className='min-w-96'>
+        <div className='min-w-96 animate-slide-down-2s'>
           <ProfileBox />
         </div>
       </div>
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [require("daisyui")],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      animation: {
+        'slide-down': 'slideDown 1s ease-out',
+        'slide-down-2s': 'slideDown 2s ease-out',
+      },
+    },
+  },
+  plugins: [require('daisyui')],
 }
