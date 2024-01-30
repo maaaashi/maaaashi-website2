3023e269e394e250df6417884e9e4df671053c6e
maaaashi <doyaseizin.7@gmail.com>
2024-01-30 22:41:43 +0900

    スマホサイズの時はスマホ表示

--- src/components/profile/WindowMock.tsx
+++ src/components/profile/WindowMock.tsx
@@ -1,17 +1,34 @@
 import { InfoBox } from './InfoBox'
 import { ProfileBox } from './ProfileBox'
 
-export const WindowMock = () => {
+export const MyProfileArea = () => {
   return (
-    <div className='mockup-window border bg-base-300'>
-      <div className='flex px-4 py-4 bg-base-200 gap-4'>
-        <div className='min-w-80 animate-slide-down'>
-          <InfoBox />
-        </div>
-        <div className='min-w-96 animate-slide-down-2s'>
-          <ProfileBox />
+    <>
+      <div className='mockup-window border bg-stone-300 hidden lg:block'>
+        <div className='flex px-4 py-4 bg-stone-200 gap-4'>
+          <div className='min-w-80'>
+            <InfoBox />
+          </div>
+          <div className='min-w-96'>
+            <ProfileBox />
+          </div>
         </div>
       </div>
-    </div>
+      <div className='mockup-phone block lg:hidden'>
+        <div className='camera'></div>
+        <div className='display'>
+          <div className='artboard artboard-demo phone-1'>
+            <div className=''>
+              <div className='w-full'>
+                <InfoBox />
+              </div>
+              <div className='w-full'>
+                <ProfileBox />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
