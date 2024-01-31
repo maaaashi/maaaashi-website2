98d60ce34bd75bbd0b1801ff7f2758f1f630db21
maaaashi <doyaseizin.7@gmail.com>
2024-02-01 07:30:54 +0900

    add theme controller in header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,17 @@
-import React from 'react'
+import { ThemeController } from './ThemeController'
 
 const Header = () => {
   return (
     <div className='border-b-[1px] w-screen font-bold'>
       <nav className='navbar'>
-        <button className='btn btn-ghost'>
-          <h1>{"MAAAASHI'S WEBSITE"}</h1>
-        </button>
+        <div className='flex-1'>
+          <button className='btn btn-ghost'>
+            <h1>{"MAAAASHI'S WEBSITE"}</h1>
+          </button>
+        </div>
+        <div className='flex-none'>
+          <ThemeController />
+        </div>
       </nav>
     </div>
   )
