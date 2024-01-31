97de40336644a8e1dd48caf4554e9ca3d06839b2
maaaashi <doyaseizin.7@gmail.com>
2024-01-31 18:36:22 +0900

    add responsive design

--- src/components/profile/index.tsx
+++ src/components/profile/index.tsx
@@ -4,7 +4,7 @@ import { ProfileBox } from './ProfileBox'
 export const MyProfileArea = () => {
   return (
     <>
-      <div className='mockup-window border bg-base-300 hidden lg:block'>
+      <div className='mockup-window border bg-base-300 hidden md:block'>
         <div className='flex px-4 py-4 bg-base-200 gap-4'>
           <div className='min-w-80'>
             <InfoBox />
@@ -14,12 +14,12 @@ export const MyProfileArea = () => {
           </div>
         </div>
       </div>
-      <div className='mockup-phone block lg:hidden'>
+      <div className='mockup-phone block md:hidden'>
         <div className='camera'></div>
         <div className='display'>
           <div className='artboard artboard-demo phone-1 p-2 pt-12 block overflow-y-auto bg-base-200'>
             <ProfileBox />
-            <div className='mt-4 lg:m-0'>
+            <div className='mt-4 md:m-0'>
               <InfoBox />
             </div>
           </div>
