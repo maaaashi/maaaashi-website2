c0f5b4396a09cff4425ba3363b4b588a223040f7
maaaashi <doyaseizin.7@gmail.com>
2024-01-31 18:05:50 +0900

    add vue

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
-
 import svelte from "@astrojs/svelte";
+
+import vue from "@astrojs/vue";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind(), svelte()]
+  integrations: [react(), tailwind(), svelte(), vue()]
 });
---
+++ src/components/Footer.vue
@@ -0,0 +1,6 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="sticky bottom-0">Created By Astro</div>
+  <img src="" alt="astro" />
+</template>
