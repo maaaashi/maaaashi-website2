ede2621851191d447b567de61c562cebea884ecf
maaaashi <doyaseizin.7@gmail.com>
2024-02-01 00:33:55 +0900

    add skill components

--- src/components/profile/info/Skill.tsx
+++ src/components/profile/info/Skill.tsx
@@ -1,6 +1,21 @@
 import React from 'react'
 import { BoxTitle } from '../BoxTitle'
+import { BoxBody } from '../BoxBody'
 
 export const Skill = () => {
-  return <BoxTitle text={'Skill'} />
+  return (
+    <div>
+      <BoxTitle text={'スキル'} />
+      <BoxBody>
+        <ul>
+          <li>TypeScript</li>
+          <li>Next.js</li>
+        </ul>
+        <b>資格</b>
+        <ul>
+          <li>AWS Solution Architect Associate</li>
+        </ul>
+      </BoxBody>
+    </div>
+  )
 }
