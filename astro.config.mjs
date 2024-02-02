06edba8824444284c699ee2d1b6faa2cdf0ebfe1
maaaashi <doyaseizin.7@gmail.com>
2024-02-03 08:58:17 +0900

    add prebuild

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
-import { defineConfig } from 'astro/config';
-import react from "@astrojs/react";
-import tailwind from "@astrojs/tailwind";
-import svelte from "@astrojs/svelte";
-import vue from "@astrojs/vue";
+import { defineConfig } from 'astro/config'
+import react from '@astrojs/react'
+import tailwind from '@astrojs/tailwind'
+import svelte from '@astrojs/svelte'
+import vue from '@astrojs/vue'
 
-import icon from "astro-icon";
+import icon from 'astro-icon'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind(), svelte(), vue(), icon()]
-});
+  integrations: [react(), tailwind(), svelte(), vue(), icon()],
+})
