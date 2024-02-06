11d0d6bb1f87c88b12d73fc318847d65854d6c64
maaaashi <doyaseizin.7@gmail.com>
2024-02-06 19:56:41 +0900

    move to playwrite test

--- e2e/specs/top.spec
+++ e2e/specs/top.spec
@@ -2,23 +2,21 @@
 * Topページを開く
 
 ## TOPページの表示(共通)
-* "MAAAASHI'S WEBSITE"という文字列が表示されていること
-* "ABOUT ME"という文字列が表示されていること
-* "EXPERIENCE"という文字列が表示されていること
-* "SKILL"という文字列が表示されていること
-* "Activities"という文字列が表示されていること
-* "Blogs"という文字列が表示されていること
-* ブログセクションが表示されていること
+* "MAAAASHI'S WEBSITE"という見出しが表示されていること
+* "自己紹介"という見出しが表示されていること
+* "Profile"という見出しが表示されていること
+* "Activities"という見出しが表示されていること
+* "Blogs"という見出しが表示されていること
 
 ## TOPページの表示(PC)
 * ウィンドウサイズを変更(width: "1920"px, height: "1080"px)
-* "div"タグの"mockup-window"クラスが"1"つ表示されていること
+* "div"タグの"mockup-window"クラスが表示されていること
 * "div"タグの"mockup-phone"クラスが表示されていないこと
-* PC用dark modeのスイッチが表示されていること
-* Mobile用dark modeのスイッチが表示されていないこと
+* "label"タグの"theme-controll-pc"クラスが表示されていること
+* "label"タグの"theme-controll-mobile"クラスが表示されていないこと
 
 ## TOPページの表示(スマホ)
 * ウィンドウサイズを変更(width: "375"px, height: "667"px)
-* "div"タグの"mockup-phone"クラスが"1"つ表示されていること
-* PC用dark modeのスイッチが表示されていないこと
-* Mobile用dark modeのスイッチが表示されていること
+* "div"タグの"mockup-phone"クラスが表示されていること
+* "label"タグの"theme-controll-mobile"クラスが表示されていること
+* "label"タグの"theme-controll-pc"クラスが表示されていないこと
--- e2e/tests/Top.ts
+++ e2e/tests/Top.ts
@@ -1,66 +1,47 @@
-import { Step } from 'gauge-ts'
-import { $, below, goto, text } from 'taiko'
-import assert = require('assert')
+import { Browser, Page, chromium, expect } from '@playwright/test'
+import { AfterSuite, BeforeSuite, Step } from 'gauge-ts'
 
 export default class Top {
+  private browser: Browser
+  private page: Page
+
+  @BeforeSuite()
+  public async beforeSuite() {
+    this.browser = await chromium.launch()
+    this.page = await this.browser.newPage()
+  }
+
+  @AfterSuite()
+  public async afterSuite() {
+    await this.page.close()
+    await this.browser.close()
+  }
+
+  @Step('ウィンドウサイズを変更(width: <width>px, height: <height>px)')
+  public async resizeWindow(width: string, height: string) {
+    await this.page.setViewportSize({ width: +width, height: +height })
+  }
+
   @Step('Topページを開く')
   public async openTodo() {
-    await goto('localhost:4321/')
+    await this.page.goto('localhost:4321/')
   }
 
-  @Step('<text>という文字列が表示されていること')
-  public async checkDisplay(innerText: string) {
-    const target = text(innerText)
-    const actual = await target.exists()
-    assert.ok(actual)
+  @Step('<text>という見出しが表示されていること')
+  public async checkHeadingDisplay(text: string) {
+    const target = this.page.getByRole('heading', { name: text })
+    await expect(target).toBeVisible()
   }
 
-  @Step('<tag>タグの<className>クラスが<number>つ表示されていること')
-  public async checkCount(tag: string, className: string, number: string) {
-    const target = $(`${tag}.${className}`)
-    const actual = (await target.elements()).length
-    assert.strictEqual(actual, +number)
+  @Step('<tag>タグの<className>クラスが表示されていること')
+  public async checkSelectorVisible(tag: string, className: string) {
+    const target = this.page.locator(`${tag}.${className}`)
+    await expect(target).toBeVisible()
   }
 
   @Step('<tag>タグの<className>クラスが表示されていないこと')
-  public async checkCountNone(tag: string, className: string) {
-    const target = $(`${tag}.${className}`)
-    const actual = await target.isVisible()
-    assert.ok(!actual)
-  }
-
-  @Step('PC用dark modeのスイッチが表示されていること')
-  public async checkPCDarkModeSwitch() {
-    const target = $('.theme-controll-pc')
-    const actual = await target.isVisible()
-    assert.ok(actual)
-  }
-
-  @Step('PC用dark modeのスイッチが表示されていないこと')
-  public async checkNotPCDarkModeSwitch() {
-    const target = $('.theme-controll-pc')
-    const actual = await target.isVisible()
-    assert.ok(!actual)
-  }
-
-  @Step('Mobile用dark modeのスイッチが表示されていること')
-  public async checkMobileDarkModeSwitch() {
-    const target = $('.theme-controll-mobile')
-    const actual = await target.isVisible()
-    assert.ok(actual)
-  }
-
-  @Step('Mobile用dark modeのスイッチが表示されていないこと')
-  public async checkNotMobileDarkModeSwitch() {
-    const target = $('.theme-controll-mobile')
-    const actual = await target.isVisible()
-    assert.ok(!actual)
-  }
-
-  @Step('ブログセクションが表示されていること')
-  public async checkBlogSectionView() {
-    const target = $('section.blogs')
-    const actual = await target.exists()
-    assert.ok(actual)
+  public async checkSelectorHidden(tag: string, className: string) {
+    const target = this.page.locator(`${tag}.${className}`)
+    await expect(target).not.toBeVisible()
   }
 }
