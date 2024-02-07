d612459f66e9576ede04fa55292aab06b07fc17c
maaaashi <doyaseizin.7@gmail.com>
2024-02-07 11:05:09 +0900

    update header to sticky

--- web/src/components/Header.tsx
+++ web/src/components/Header.tsx
@@ -2,7 +2,7 @@ import { ThemeController } from './ThemeController'
 
 const Header = () => {
   return (
-    <div className='border-b-[1px] w-screen font-bold'>
+    <div className='border-b-[1px] w-screen font-bold sticky top-0 z-10 bg-base-200'>
       <nav className='navbar px-4'>
         <div className='flex-1'>
           <h1 id='maaaashi-website'>{"MAAAASHI'S WEBSITE"}</h1>
