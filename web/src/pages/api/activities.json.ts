2f9f9b68e85ccf25bc931d21f6330bbd7dae289d
maaaashi <doyaseizin.7@gmail.com>
2024-02-13 08:03:07 +0900

    add api

---
+++ web/src/pages/api/activities.json.ts
@@ -0,0 +1,14 @@
+import type { APIRoute } from 'astro'
+
+export const GET: APIRoute = async () => {
+  const response = await fetch(
+    'https://qiita.com/api/v2/items?per_page=100&query=user:maaaashi',
+  )
+  const articles = await response.json()
+  console.log(await response.json())
+  return new Response(
+    JSON.stringify({
+      articles,
+    }),
+  )
+}
