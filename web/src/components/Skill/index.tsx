05f9db9e252d4d3cb504b5094a1c468f574bf97d
maaaashi <doyaseizin.7@gmail.com>
2024-02-16 07:31:37 +0900

    add image pattern

--- web/src/components/Skill/index.tsx
+++ web/src/components/Skill/index.tsx
@@ -1,9 +1,20 @@
 export const Skill = () => {
   return (
-    <div>
+    <div className='flex justify-center'>
       <img
-        src='https://skillicons.dev/icons?i=angular,apollo,astro,aws,azure,babel,bash,bootstrap,css,dart,discord,docker,dynamodb,electron,figma,firebase,git,github,githubactions,github,graphql,idea,js,jest,kotlin,kubernetes,linux,md,materialui,maven,mysql,nestjs,nextjs,nginx,nodejs,nuxtjs,postgres,postman,prisma,py,rails,react,redux,regex,ruby,rust,sass,sqlite,supabase,selenium,sketchup,svelte,svg,tailwind,threejs,twitter,ts,vercel,vite,vitest,vscode,vue,webpack,'
+        src='https://skillicons.dev/icons?i=angular,apollo,astro,aws,azure,babel,bash,bootstrap,css,dart,discord,docker,dynamodb,electron,figma,firebase,git,github,githubactions,github,graphql,idea,js,jest,kotlin,kubernetes,html,linux,md,materialui,maven,mysql,nestjs,nextjs,nginx,nodejs,nuxtjs,postgres,postman,prisma,py,rails,react,redux,regex,ruby,rust,sass,sqlite,supabase,selenium,sketchup,svelte,svg,tailwind,threejs,twitter,ts,vercel,vite,vitest,vscode,vue,webpack,'
         alt=''
+        className='hidden md:block'
+      />
+      <img
+        src='https://skillicons.dev/icons?i=angular,apollo,astro,aws,azure,babel,bash,bootstrap,css,dart,discord,docker,dynamodb,electron,figma,firebase,git,github,githubactions,github,graphql,idea,js,jest,kotlin,kubernetes,html,linux,md,materialui,maven,mysql,nestjs,nextjs,nginx,nodejs,nuxtjs,postgres,postman,prisma,py,rails,react,redux,regex,ruby,rust,sass,sqlite,supabase,selenium,sketchup,svelte,svg,tailwind,threejs,twitter,ts,vercel,vite,vitest,vscode,vue,webpack&perline=10'
+        alt=''
+        className='hidden sm:block md:hidden'
+      />
+      <img
+        src='https://skillicons.dev/icons?i=angular,apollo,astro,aws,azure,babel,bash,bootstrap,css,dart,discord,docker,dynamodb,electron,figma,firebase,git,github,githubactions,github,graphql,idea,js,jest,kotlin,kubernetes,html,linux,md,materialui,maven,mysql,nestjs,nextjs,nginx,nodejs,nuxtjs,postgres,postman,prisma,py,rails,react,redux,regex,ruby,rust,sass,sqlite,supabase,selenium,sketchup,svelte,svg,tailwind,threejs,twitter,ts,vercel,vite,vitest,vscode,vue,webpack&perline=5'
+        alt=''
+        className='sm:hidden'
       />
     </div>
   )
