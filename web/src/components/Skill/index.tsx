514b45d714f09009fbdd6e5ada5000391c70485a
maaaashi <doyaseizin.7@gmail.com>
2024-02-16 00:54:56 +0900

    add skill image

--- web/src/components/Skill/index.tsx
+++ web/src/components/Skill/index.tsx
@@ -1,8 +1,10 @@
 export const Skill = () => {
   return (
     <div>
-      <img src='/typescript.svg' alt='' />
-      作成中...
+      <img
+        src='https://skillicons.dev/icons?i=angular,apollo,astro,aws,azure,babel,bash,bootstrap,css,dart,discord,docker,dynamodb,electron,figma,firebase,git,github,githubactions,github,graphql,idea,js,jest,kotlin,kubernetes,linux,md,materialui,maven,mysql,nestjs,nextjs,nginx,nodejs,nuxtjs,postgres,postman,prisma,py,rails,react,redux,regex,ruby,rust,sass,sqlite,supabase,selenium,sketchup,svelte,svg,tailwind,threejs,twitter,ts,vercel,vite,vitest,vscode,vue,webpack,'
+        alt=''
+      />
     </div>
   )
 }
