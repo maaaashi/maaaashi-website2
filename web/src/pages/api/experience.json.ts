b4202b779ab904a91403cf2bd4c0da8a9073548b
maaaashi <doyaseizin.7@gmail.com>
2024-02-16 13:05:51 +0900

    :gear:

--- web/src/pages/api/experience.json.ts
+++ web/src/pages/api/experience.json.ts
@@ -26,7 +26,7 @@ export const GET: APIRoute = async () => {
           date: new Date('2023-11-01'),
           topic: '株式会社ユーザベース 入社',
           content:
-            'In April 2016, I embarked on my academic journey in architecture, dedicating four years to the study of building design and construction. Throughout this period, I was constantly challenged by rigorous coursework and demanding projects, which made it an intensely demanding four years. Through this journey, I realized that while architecture offered valuable lessons and experiences, my true passion lay elsewhere. This realization led me to pivot towards the IT industry, an area where my interest had significantly grown. Motivated by this newfound direction, I embarked on job hunting in the IT sector, eager to explore the opportunities it presented.',
+            'Following an invitation from a former colleague, I joined Userbase Inc., a company focused on developing a platform for economic information. Here, I am involved in agile development processes and fully embrace pair programming practices.',
         },
         {
           date: new Date(),
