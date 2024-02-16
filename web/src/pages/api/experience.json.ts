4da0afad207feed7c5f8f5339c435a75347be3b7
maaaashi <doyaseizin.7@gmail.com>
2024-02-16 12:57:53 +0900

    経歴修正

--- web/src/pages/api/experience.json.ts
+++ web/src/pages/api/experience.json.ts
@@ -14,13 +14,13 @@ export const GET: APIRoute = async () => {
           date: new Date('2020-04-01'),
           topic: '木村情報技術株式会社 入社',
           content:
-            'In April 2016, I embarked on my academic journey in architecture, dedicating four years to the study of building design and construction. Throughout this period, I was constantly challenged by rigorous coursework and demanding projects, which made it an intensely demanding four years. Through this journey, I realized that while architecture offered valuable lessons and experiences, my true passion lay elsewhere. This realization led me to pivot towards the IT industry, an area where my interest had significantly grown. Motivated by this newfound direction, I embarked on job hunting in the IT sector, eager to explore the opportunities it presented.',
+            "In April 2020, I joined Kimura Information Technology Co., Ltd., an IT company based in Saga Prefecture. For the initial six months, I spent my time in Saga, training as a Web Application Engineer. Following this period, I moved to the company's Tokyo branch, where I engaged in the development of AI chatbots.",
         },
         {
           date: new Date('2023-10-31'),
           topic: '木村情報技術株式会社 退社',
           content:
-            'In April 2016, I embarked on my academic journey in architecture, dedicating four years to the study of building design and construction. Throughout this period, I was constantly challenged by rigorous coursework and demanding projects, which made it an intensely demanding four years. Through this journey, I realized that while architecture offered valuable lessons and experiences, my true passion lay elsewhere. This realization led me to pivot towards the IT industry, an area where my interest had significantly grown. Motivated by this newfound direction, I embarked on job hunting in the IT sector, eager to explore the opportunities it presented.',
+            'In October 2023, I left Kimura Information Technology Co., Ltd.',
         },
         {
           date: new Date('2023-11-01'),
@@ -31,8 +31,7 @@ export const GET: APIRoute = async () => {
         {
           date: new Date(),
           topic: '現在',
-          content:
-            'In April 2016, I embarked on my academic journey in architecture, dedicating four years to the study of building design and construction. Throughout this period, I was constantly challenged by rigorous coursework and demanding projects, which made it an intensely demanding four years. Through this journey, I realized that while architecture offered valuable lessons and experiences, my true passion lay elsewhere. This realization led me to pivot towards the IT industry, an area where my interest had significantly grown. Motivated by this newfound direction, I embarked on job hunting in the IT sector, eager to explore the opportunities it presented.',
+          content: '',
         },
       ],
     }),
