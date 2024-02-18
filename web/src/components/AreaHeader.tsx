3d8a0b04c9b823ce1aa01cde33f624d51f01b792
maaaashi <doyaseizin.7@gmail.com>
2024-02-19 07:12:46 +0900

    :gear: design

--- web/src/components/AreaHeader.tsx
+++ web/src/components/AreaHeader.tsx
@@ -7,7 +7,7 @@ interface Props {
 export const AreaHeader: FC<Props> = ({ text }) => {
   return (
     <div className='p-4 w-full bg-transparent h-44 flex justify-start items-end'>
-      <h2 className='p-4 font-bold text-2xl w-fit h-fit bg-base-300 rounded-lg'>
+      <h2 className='px-4 py-2 font-bold text-xl w-fit h-fit bg-base-300 rounded-lg'>
         {text}
       </h2>
     </div>
