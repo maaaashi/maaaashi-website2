c33c1b69c4019c93c257c332a362bcf7aa95b4d6
maaaashi <doyaseizin.7@gmail.com>
2024-02-18 10:04:23 +0900

    :gear: design

--- web/src/components/AreaHeader.tsx
+++ web/src/components/AreaHeader.tsx
@@ -1,21 +1,15 @@
-import { useRef, type FC, useEffect } from 'react'
+import { type FC } from 'react'
 
 interface Props {
   text: string
-  backGroundColor?: string
 }
 
-export const AreaHeader: FC<Props> = ({ text, backGroundColor }) => {
-  const ref = useRef<HTMLHeadingElement>(null)
-
-  useEffect(() => {}, [])
-
+export const AreaHeader: FC<Props> = ({ text }) => {
   return (
-    <h2
-      className={`font-bold text-2xl p-4 sticky top-0 w-fit z-10 ${backGroundColor || 'bg-base-100'}`}
-      ref={ref}
-    >
-      {text}
-    </h2>
+    <div className='p-4 w-full bg-transparent h-44 flex justify-start items-end'>
+      <h2 className='p-4 font-bold text-2xl w-fit h-fit bg-base-300 rounded-lg'>
+        {text}
+      </h2>
+    </div>
   )
 }
