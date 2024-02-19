e5d67e304c7d69b0290a564613cbf30bf02206e8
maaaashi <doyaseizin.7@gmail.com>
2024-02-19 12:28:21 +0900

    add page up button

---
+++ web/src/components/PageUpButton.tsx
@@ -0,0 +1,9 @@
+import { IoMdArrowRoundUp } from 'react-icons/io'
+
+export const PageUpButton = () => {
+  return (
+    <button className='btn btn-circle absolute bottom-5 right-10'>
+      <IoMdArrowRoundUp />
+    </button>
+  )
+}
