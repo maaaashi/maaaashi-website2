aee0af79cfd3a3ad5ed50a5e826d8ebbad427ccc
maaaashi <doyaseizin.7@gmail.com>
2024-02-20 07:44:22 +0900

    add contact info

---
+++ web/src/components/Qiita.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  export let size = 16;
+</script>
+
+<style>
+  :global(img) {
+    width: var(--size);
+    height: var(--size);
+    object-fit: contain;
+  }
+</style>
+
+<img src="/qiita-logo.png" alt="" style={`--size: ${size}px;`}>
