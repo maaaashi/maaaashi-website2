33d1f9752c33d3e25f4d4034c65b6cda1adc6dbc
maaaashi <doyaseizin.7@gmail.com>
2024-02-25 00:36:19 +0900

    :octocat: add pageup button

--- web/src/components/PageUpButton.tsx
+++ web/src/components/PageUpButton.tsx
@@ -1,9 +1,26 @@
 import { IoMdArrowRoundUp } from 'react-icons/io'
 
 export const PageUpButton = () => {
+  const clickHandler = () => {
+    const main = document.getElementById('main')
+    main?.scrollTo({ top: 0, behavior: 'smooth' })
+  }
+
   return (
-    <button className='btn btn-circle btn-primary absolute bottom-5 right-10'>
-      <IoMdArrowRoundUp />
-    </button>
+    <>
+      <button
+        className='hidden md:flex md:items-center btn btn-primary absolute bottom-5 right-10 z-20'
+        onClick={clickHandler}
+      >
+        <span className='block'>Page Up</span>
+        <IoMdArrowRoundUp />
+      </button>
+      <button
+        className='md:hidden btn btn-circle btn-primary absolute bottom-5 right-10 z-20'
+        onClick={clickHandler}
+      >
+        <IoMdArrowRoundUp />
+      </button>
+    </>
   )
 }
