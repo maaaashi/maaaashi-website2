c3456238d4048e40eb2e48fdd9e513e074550560
maaaashi <doyaseizin.7@gmail.com>
2024-11-07 22:15:41 +0900

    画像最適化

--- web/src/components/TopImage.tsx
+++ web/src/components/TopImage.tsx
@@ -2,7 +2,7 @@ export const TopImage = () => {
   return (
     <div>
       <div className='fixed -z-10 top-16 left-1/2 -translate-x-1/2 w-max'>
-        <img className='h-screen object-cover' src='/top_no_gazou.png' alt='' />
+        <img className='h-screen object-cover' src='/top.webp' alt='' />
       </div>
       <div className='w-full h-screen relative'>
         <span className='mb-4 text-3xl font-extrabold text-gray-400 dark:text-white md:text-5xl lg:text-6xl left-12 top-1/2 -translate-y-12 absolute'>
